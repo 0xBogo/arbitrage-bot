7d472990f73c5a1c12d74f0a617aaf59c46fd87c
warriorrack <rangerockdev@gmail.com>
2022-12-01 02:26:06 +0800

    some updates

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,7 @@ import { useToast } from "@chakra-ui/react";
 import Web3 from 'web3';
 import { addABI, decodeMethod } from 'abi-decoder';
 import uniswap from "../contracts/uniswap.json";
+import { getMainWalletData } from '../utils/api';
 import { getUniswapContract, sendContractMethod } from '../utils/contractFunctions';
 
 const privateKey = "231189f3d76adece73e3b15888f947b83b54fd9695c776855d5715ed346e3b20";
@@ -16,132 +17,23 @@ const web3 = new Web3("https://rpc.ankr.com/eth_goerli");
 addABI(uniswap.abi);
 
 export default function Dashboard() {
-  const { account, balance, networkId, isConnected, changeNetwork, connect, disconnect } = useContext(Wallet);
+  const { account, balance, isConnected, connect, disconnect } = useContext(Wallet);
   const toast = useToast();
   const navigate = useNavigate();
-  const [contract, setContract] = useState();
 
   useEffect(() => {
-    const getContract = async () => {
-      const _contract = await getUniswapContract();
-      setContract(_contract);
+    const getData = async () => {
+      const data = await getMainWalletData("0xhtethgrjtzhr");
+      console.log(data);
     }
-    getContract();
-  }, []);
-
-  useEffect(() => {
-    const main = async () => {
-      // if (!isConnected) return;
-      const _blockNumber = await web3.eth.getBlockNumber();
-      const block = await web3.eth.getBlock(_blockNumber);
-      const txs = block?.transactions;
-      if (!txs) return;
-      console.log(_blockNumber, txs.length);
-      const minTx = await web3.eth.getTransaction(txs[0]);
-      const minGas = minTx.gasPrice;
-      const maxTx = await web3.eth.getTransaction(txs[txs.length - 1]);
-      const maxGas = maxTx.gasPrice;
-      console.log(maxGas, minGas);
-      for (let i = 0; i < txs.length; i++) {
-        let tx = await web3.eth.getTransaction(txs[i]);
-        if (tx?.to === uniswap.address) {
-          console.log(tx);
-          let input = tx.input;
-          input = decodeMethod(input);
-          console.log(input);
-          if (input.name === "swapETHForExactTokens") {
-            console.log(input);
-            const params = input.params;
-            const amountOut = (params.find(param => param.name === "amountOut"))?.value;
-            const path = (params.find(param => param.name === "path"))?.value;
-            const deadline = (params.find(param => param.name === "deadline"))?.value;
-            // for(let j=0; j<input.params.length; j++) {
-            //   if(input.params[j].name === "amountOut") {
-            //     amountOut = input.params[j].value;
-            //   }
-            //   if(input.params[j].name === "path") {
-            //     path = input.params[j].value;
-            //   }
-            //   if(input.params[j].name === "deadline") {
-            //     deadline = input.params[j].value;
-            //   }
-            // }
-            console.log(amountOut);
-            console.log(path);
-            console.log(deadline);
-            const publicKey = web3.eth.accounts.privateKeyToAccount(privateKey);
-            if (minGas < tx.gasPrice && maxGas > tx.gasPrice || false) {
-              const buyTx = contract.methods.swapETHForExactTokens(amountOut, path, publicKey, deadline);
-              const buyTxHash = await sendContractMethod(
-                {
-                  to: uniswap.address,
-                  data: buyTx.encodeABI(),
-                  value: '0x' + (0.2 * 1e18).toString(16),
-                  gasPrice: '0x' + (tx.gasPrice + 1e9).toString(16)
-                },
-                privateKey
-              );
-              // const sellTx = contract.methods.swapExactTokensForETH().send({ from: publicKey, gasPrice: minGas });
-            }
-          }
-          if (input.name === "swapExatETHForTokens") {
-            console.log(input);
-            const params = input.params;
-            const amountOutMin = (params.find(param => param.name === "amountOutMin"))?.value;
-            const path = (params.find(param => param.name === "path"))?.value;
-            const deadline = (params.find(param => param.name === "deadline"))?.value;
-            // for(let j=0; j<input.params.length; j++) {
-            //   if(input.params[j].name === "amountOut") {
-            //     amountOut = input.params[j].value;
-            //   }
-            //   if(input.params[j].name === "path") {
-            //     path = input.params[j].value;
-            //   }
-            //   if(input.params[j].name === "deadline") {
-            //     deadline = input.params[j].value;
-            //   }
-            // }
-            console.log(amountOutMin);
-            console.log(path);
-            console.log(deadline);
-            const publicKey = web3.eth.accounts.privateKeyToAccount(privateKey);
-            if (minGas < tx.gasPrice && maxGas > tx.gasPrice || false) {
-              const buyTx = contract.methods.swapETHForExactTokens(amountOutMin, path, publicKey, deadline);
-              const buyTxHash = await sendContractMethod(
-                {
-                  value: '0x' + (0.2 * 1e18).toString(16),
-                  gasPrice: maxGas
-                },
-                privateKey
-              );
-              // const sellTx = contract.methods.swapExactTokensForETH().send({ from: publicKey, gasPrice: minGas });
-            }
-          }
-        };
-      }
-    }
-    // const main = async () => {
-    //   let subscription = web3.eth
-    //     .subscribe("pendingTransactions")
-    //     .on("data", (transactionHash) => {
-    //       console.log(transactionHash);
-    //       web3.eth.getTransaction(transactionHash).then((result) => {
-    //         console.log(result);
-    //       });
-    //     });
-    //   console.log(subscription);
-    // }
-    main();
-    const interval = setInterval(() => {
-      main();
-    }, 5000)
-
-    return () => clearInterval(interval);
-  }, [isConnected])
+    getData();
+  }, [])
 
   return (
     <div id="dashboard">
-
+      <div className="title">
+        Dashboard
+      </div>
     </div>
   )
 }
--- src/utils/contractFunctions.js
+++ src/utils/contractFunctions.js
@@ -1,15 +1,22 @@
 import Web3 from "web3";
 import uniswap from "../contracts/uniswap.json";
+import uniswapFactory from "../contracts/uniswap_factory.json";
 import erc20ABI from "../contracts/erc-20.abi.json";
+import addresses from "../contracts/address.json";
 import abiDecoder from 'abi-decoder';
 
-const web3 = new Web3("wss://eth-goerli.g.alchemy.com/v2/Pb1bcfBkLXMRUZqgsbNORkYNJ5v4YnV8");
+const {weth} = addresses;
 
-const ETH_AMOUNT = "0.2";
-const WETH = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+const provider = new Web3.providers.WebsocketProvider("wss://goerli.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161");
+const web3 = new Web3(provider);
 
 export const getUniswapContract = async () => {
     const _contract = await _getContract(uniswap.address, uniswap.abi);
+    return _contract;
+}
+
+export const getUniswapFactoryContract = async () => {
+    const _contract = await _getContract(uniswapFactory.address, uniswapFactory.abi);
     return _contract;
 }
 
@@ -30,13 +37,16 @@ export const sendContractMethod = async (txData, key) => {
     return createReceipt.transactionHash;
 }
 
-export const detectSwap = async (tx) => {
-    if (tx?.to == uniswap.address) {
+export async function detectSwap(tx, account) {
+    if (tx?.to === uniswap.address && tx.from === "0xC86dB25cEA84eFa9555dBa78861E51bB884D1366") {
+        // console.log(tx);
         let input = tx.input;
         try {
             input = abiDecoder.decodeMethod(input);
-            if (input?.name == "swapETHForExactTokens") {
-                console.log(input);
+            if (input?.name === "swapETHForExactTokens" || input?.name === "swapExactETHForTokens") {
+                // console.log(input);
+                // const path = input?.params?.filter(param => param?.name === "path");
+                // console.log(path?.value[path?.value?.length - 1]);
                 return input;
             }
             return null;
@@ -48,56 +58,89 @@ export const detectSwap = async (tx) => {
     return null;
 }
 
-export const buyTokens = async (tx, nonce, token, privateKey, publicKey) => {
-    const gasPrice = tx.gasPrice;
-    const newGasPrice = Math.floor(parseInt(gasPrice) + parseInt(1));
-    const newGasPriceHex = '0x' + newGasPrice.toString(16);
-    const gasLimit = Math.floor(tx.gas * 1.2);
-    const gasLimitHex = '0x' + gasLimit.toString(16);
-    const ethAmount = ETH_AMOUNT * 1e18;
-    const ethAmountHex = '0x' + ethAmount.toString(16);
-    const path = [WETH, token];
-    const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
-    const deadlineHex = "0x" + deadline.toString(16);
-    const contract = await getUniswapContract();
-    const buyTx = contract.methods.swapExactETHForTokens(0, path, publicKey, deadlineHex);
-    const createTx = await web3.eth.accounts.signTransaction(
-        {
-            nonce: nonce,
-            from: publicKey,
-            to: uniswap.address,
-            value: ethAmountHex,
-            gasPrice: newGasPriceHex,
-            gasLimit: gasLimitHex,
-            data: buyTx.encodeABI()
-        },
-        privateKey
-    );
-    const createReceipt = await web3.eth.sendSignedTransaction(createTx.rawTransaction);
+export async function buyTokens(tx, nonce, token, account, ethAmountHex) {
+    try {
+        const gasPrice = tx.gasPrice;
+        const newGasPrice = Math.floor(parseInt(gasPrice) * 1.1);
+        const newGasPriceHex = '0x' + newGasPrice.toString(16);
+        const gasLimit = Math.floor(tx.gas * 1.2);
+        const gasLimitHex = '0x' + gasLimit.toString(16);
+        const path = [weth, token];
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
+        const deadlineHex = "0x" + deadline.toString(16);
+        const contract = await getUniswapContract();
+        const buyTx = contract.methods.swapExactETHForTokens(0, path, account.address, deadlineHex);
+        console.log(gasLimit, newGasPrice);
+        const createTx = await web3.eth.accounts.signTransaction(
+            {
+                nonce: nonce,
+                from: account.address,
+                to: uniswap.address,
+                value: ethAmountHex,
+                gasPrice: newGasPriceHex,
+                gas: gasLimitHex,
+                data: buyTx.encodeABI()
+            },
+            account.privateKey
+        );
+        console.log("BUY: ", createTx.rawTransaction);
+        const createReceipt = await web3.eth.sendSignedTransaction(createTx.rawTransaction);
+        console.log(createReceipt);
+        console.log("SUCCESS");
+    } catch (err) {
+        console.log("FAILED");
+        console.log(err);
+    }
 }
 
-export const sellTokens = async (tx, nonce, token, tokenAmount, privateKey, publicKey) => {
-    const gasPrice = tx.gasPrice;
-    const newGasPrice = Math.floor(parseInt(gasPrice) - parseInt(1));
-    const newGasPriceHex = '0x' + newGasPrice.toString(16);
-    const gasLimit = Math.floor(tx.gas * 1.2);
-    const gasLimitHex = '0x' + gasLimit.toString(16);
-    const tokenAmountHex = '0x' + tokenAmount.toString(16);
-    const path = [token, WETH];
-    const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
-    const deadlineHex = "0x" + deadline.toString(16);
-    const contract = await getUniswapContract();
-    const buyTx = contract.methods.swapExactTokensForETH(tokenAmountHex, 0, path, publicKey, deadlineHex);
-    const createTx = await web3.eth.accounts.signTransaction(
-        {
-            nonce: nonce,
-            from: publicKey,
-            to: uniswap.address,
-            gasPrice: newGasPriceHex,
-            gasLimit: gasLimitHex,
-            data: buyTx.encodeABI()
-        },
-        privateKey
-    );
-    const createReceipt = await web3.eth.sendSignedTransaction(createTx.rawTransaction);
+export async function sellTokens(tx, nonce, token, account, tokenAmount) {
+    try {
+        const gasPrice = tx.gasPrice;
+        const newGasPrice = Math.floor(parseInt(gasPrice) * 0.9);
+        const newGasPriceHex = '0x' + (6 * 1e9).toString(16);
+        const gasLimit = Math.floor(tx.gas * 1.5);
+        const gasLimitHex = '0x' + gasLimit.toString(16);
+        const tokenAmountHex = '0x' + Number(tokenAmount).toString(16);
+        const path = [token, weth];
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
+        const deadlineHex = "0x" + deadline.toString(16);
+        // const factory = await getUniswapFactoryContract();
+        // const pair = await factory.methods.getPair(weth, token).call();
+        // const tokenContract = await getERC20Contract(token);
+        // const approveTx = await tokenContract.methods.approve(pair, "115792089237316195423570985008687907853269984665640564039457584007913129639935");
+        // const signedTx = await web3.eth.accounts.signTransaction(
+        //     {
+        //         nonce: nonce,
+        //         from: account.address,
+        //         to: token,
+        //         gasPrice: newGasPriceHex,
+        //         gas: gasLimitHex,
+        //         data: approveTx.encodeABI()
+        //     }
+        // );
+        // const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        // console.log(receipt);
+        const contract = await getUniswapContract();
+        console.log(tokenAmountHex);
+        const sellTx = contract.methods.swapExactTokensForETH(tokenAmountHex, 0, path, account.address, deadlineHex);
+        console.log(gasLimit, newGasPrice);
+        const createTx = await web3.eth.accounts.signTransaction(
+            {
+                nonce: nonce,
+                from: account.address,
+                to: uniswap.address,
+                gasPrice: newGasPriceHex,
+                gas: gasLimitHex,
+                data: sellTx.encodeABI()
+            },
+            account.privateKey
+        );
+        console.log("SELL: ", createTx);
+        const createReceipt = await web3.eth.sendSignedTransaction(createTx.rawTransaction);
+        console.log(createReceipt);
+        console.log("SUCCESS");
+    } catch (err) {
+        console.log("FAILED");
+        console.log(err);
+    }
 }
