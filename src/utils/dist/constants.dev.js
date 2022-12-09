011979629eb0817b3658f946d6d1ffb64228afec
CC-Peter <shnj1902@163.com>
2022-12-09 22:18:01 +0800

    updated contracts data and added filter

--- src/utils/dist/constants.dev.js
+++ src/utils/dist/constants.dev.js
@@ -3,14 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.GET_TOKEN = exports.GET_ALL_TOKENS = exports.GRAPHQL_URL = void 0;
+exports.GET_ALL_TOKENS = exports.GRAPHQL_URL = void 0;
 var GRAPHQL_URL = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2";
 exports.GRAPHQL_URL = GRAPHQL_URL;
-var GET_ALL_TOKENS = "{\n    tokens (first: 50, skip: 5000) {\n        id\n        symbol\n        name\n        decimals\n        totalSupply\n        tradeVolume\n        tradeVolumeUSD\n        untrackedVolumeUSD\n        totalLiquidity\n        tokenDayData {\n            priceUSD\n            dailyVolumeUSD\n        }\n    }\n}";
+var GET_ALL_TOKENS = "{\n    tokens(first: 500, skip: 5000) {\n        id\n        name\n        symbol\n        totalSupply\n        totalLiquidity\n        decimals\n        tokenDayData(first: 1) {\n            dailyVolumeUSD\n            priceUSD\n        }\n        pairBase {\n            id\n        }\n    }\n}";
 exports.GET_ALL_TOKENS = GET_ALL_TOKENS;
-
-var GET_TOKEN = function GET_TOKEN(address) {
-  return "{\n    token(id: \"".concat(address, "\") {\n        id\n        name\n        symbol\n        decimals\n        totalSupply\n        totalLiquidity\n        tradeVolume\n        tradeVolumeUSD\n        untrackedVolumeUSD\n        tokenDayData {\n            priceUSD\n            dailyVolumeUSD\n        }\n    }\n}");
-};
-
-exports.GET_TOKEN = GET_TOKEN;
