cd3404a77bc91e8ce194484deb14bd0b5f25d5c9
CC-Peter <shnj1902@163.com>
2022-12-13 10:26:57 +0800

    updated contracts page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,15 @@
 export const GRAPHQL_URL = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2";
 export const GET_ALL_TOKENS = `{
-    tokens(first: 500, skip: 5000) {
+    tokens(first: 100, skip: 5000) {
         id
         name
         symbol
         totalSupply
-        totalLiquidity
         decimals
-        tokenDayData(first: 1) {
-            dailyVolumeUSD
-            priceUSD
-        }
         pairBase {
             id
         }
     }
 }`;
+
+export const DEXSCREENER_URL = (pair) => `https://api.dexscreener.com/latest/dex/pairs/ethereum/${pair}`;
